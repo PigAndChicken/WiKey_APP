e5408ef15e2b3609f2017eaff16bfe90bf72e06f
ShellyHsueh <hsueh821203@gmail.com>
2017-12-02 23:09:44 +0800

    try

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,9 @@
 require 'roda'
-require 'econfig'
 
 module WiKey
   # configuration for the App
   class App < Roda
     plugin :environments
-    
-    extend Econfig::Shortcut
-    Econfig.env = environment.to_s
-    Econfig.root = '.'
     
     configure :development do
       # Allows running reload! in pry to restart entire app
