180080d0246cbea39c4522d58d1c34594040305e
ShellyHsueh <hsueh821203@gmail.com>
2017-12-12 17:12:59 +0800

    small modification

--- infrastructure/api_gateway.rb
+++ infrastructure/api_gateway.rb
@@ -17,13 +17,13 @@ module WiKey
     def create_topic(topic_name)
       call_api(:post, ['topic', topic_name])
     end
-    
+
     def summaries(topic_name, catalog_name)
       call_api(:get, ['summaries', topic_name, catalog_name])
     end
 
     def call_api(method, resources)
-      url_route = [@config.api_url, resources].flatten.join'/'
+      url_route = [@config.API_URL, resources].flatten.join'/'
 
       result = HTTP.send(method, url_route)
       result.to_s
