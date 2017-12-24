7be1ba0e866201b46ee96fdfa22309efb53591d6
ShellyHsueh <hsueh821203@gmail.com>
2017-12-24 23:19:50 +0800

    test

--- application/views/processing_view.rb
+++ application/views/processing_view.rb
@@ -7,7 +7,7 @@ module WiKey
       end
 
       def ws_channel_id
-        JSON.parse(@result.message)['message'][2]['id']
+        JSON.parse(@result.message)['message']['id']
       end
 
       def ws_host
