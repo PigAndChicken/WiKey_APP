d3ab207304593cf4b94e5fdf6fa76a2f59ed649f
ShellyHsueh <hsueh821203@gmail.com>
2017-12-30 19:53:32 +0800

    remove redundant rows

--- application/views/subject_contents.rb
+++ application/views/subject_contents.rb
@@ -13,17 +13,9 @@ module WiKey
       def catalogs
         @topic_info['catalogs'].map { |c| c['name'] } # [c1, c2, c3]
       end
-=begin
-      def catalog
-        @topic_info['paragraphs'][0]['catalog'] # "catalog_name"
-      end
-=end
+
       def summaries
         @topic_info['paragraphs'].map { |p| p['content'] } # [p1, p2, p3]
-      end
-
-      def link_to_topic_summaries
-        "/summaries/#{topic}/#{catalog}"
       end
     end
   end
