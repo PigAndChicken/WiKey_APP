e3848eb3ba5d4aec7d16dadb310dcfc39fcc897f
ShellyHsueh <hsueh821203@gmail.com>
2018-01-03 20:06:25 +0800

    forms fixed

--- application/controllers/topic.rb
+++ application/controllers/topic.rb
@@ -4,15 +4,18 @@ module WiKey
     # GET or POST /topic
     route('topic') do |routing|
       routing.post do
-        topic_name = Forms::InputReg.call(routing.params)
+        topic_input = Forms::InputReg.call(routing.params)
 
-        result = CheckTopic.new.call(topic_name).value
-        if result.message.include? 'Remote article not found'
+        result = CheckTopic.new.call(topic_input).value
+
+        if result.include? 'special notations'
+          flash[:error] = 'Invalid input started/ended with special notations.'
+          routing.redirect '/'
+        elsif result.include? 'Remote article not found'
           flash[:error] = 'Not exists in Wikipedia'
           routing.redirect '/'
         else
-          topic_info = ArticleRepresenter.new(OpenStruct.new)
-                                         .from_json result.message
+          topic_info = ArticleRepresenter.new(OpenStruct.new).from_json result
 
           subject_contents = Views::SubjectContents.new(topic_info)
           view 'topic_summary', locals: { subject_contents: subject_contents }
--- application/services/check_topic.rb
+++ application/services/check_topic.rb
@@ -5,24 +5,33 @@ module WiKey
   class CheckTopic
     include Dry::Transaction
 
+    step :check_if_valid
     step :check_if_exists
     step :create_topic
+
+    def check_if_valid(topic_input)
+      if topic_input.errors[:topic].nil?
+        Right(topic_input.output[:topic])
+      else
+        Left(topic_input.errors[:topic][0])
+      end
+    end
 
     def check_if_exists(topic_name)
       topic_info = ApiGateway.new.topic(topic_name)
       if topic_info.message.include? 'Could not find'
         Right(topic_name) # not loaded yet, go next step to create
       else
-        Left(topic_info) # out with info
+        Left(topic_info.message) # out with info
       end
     end
 
     def create_topic(topic_name)
       topic_info = ApiGateway.new.create_topic(topic_name)
       if topic_info.message.include? 'not found'
-        Left(topic_info) # not exist in Wikipedia, out with error msg
+        Left(topic_info.message) # not exist in Wikipedia, out with error msg
       else
-        Right(topic_info)
+        Right(topic_info.message)
       end
     end
   end
