421dfe2b71861804db82d93f699a11b860b30903
ShellyHsueh <hsueh821203@gmail.com>
2018-01-07 10:23:07 +0800

    origin btn for default page

--- application/controllers/topic.rb
+++ application/controllers/topic.rb
@@ -17,7 +17,13 @@ module WiKey
           topic_info = ArticleRepresenter.new(OpenStruct.new).from_json result
           subject_contents = Views::SubjectContents.new(topic_info)
 
+          origin_info = ApiGateway.new.paragraphs(topic_input.output[:topic], 'default')
+          origin_info = ArticleRepresenter.new(OpenStruct.new)
+                                          .from_json origin_info.message
+          origin_contents = Views::OriginContents.new(origin_info)
+
           view 'topic_summary', locals: { subject_contents: subject_contents,
+                                          origin_contents: origin_contents,
                                           topic_page: true }
         end
       end
